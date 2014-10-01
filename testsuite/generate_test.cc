b753bcbce5da575acb8354aecec03366ef1af04c
Diptorup Deb <diptorup@cs.unc.edu>
2014-09-30 21:03:07 -0400

    Some more test cases

--- testsuite/generate_test.cc
+++ testsuite/generate_test.cc
@@ -20,19 +20,19 @@ using namespace std;
 void create_test_dir()
 {
     struct stat st = {0};
-    if (stat("testscripts", &st) == -1) {
-        mkdir("testscripts", 0700);
+    if (stat("intrinsic_fns", &st) == -1) {
+        mkdir("intrinsic_fns", 0700);
     }
     else {
-        remove("testscripts");
-        mkdir("testscripts", 0700);
+        remove("intrinsic_fns");
+        mkdir("intrinsic_fns", 0700);
     }
 }
 
 
 void generate_FnMadd()
 {
-    ofstream out("testscripts/test_gen_fnMadd.h");
+    ofstream out("intrinsic_fns/gen_fnMadd.h");
 
     InstVector ivector;  
     FVec avec("avec");
@@ -73,8 +73,189 @@ void generate_FnMadd()
 
 }
 
+void generate_FMadd()
+{
+    ofstream out("intrinsic_fns/gen_fMadd.h");
+
+    InstVector ivector;  
+    FVec avec("avec");
+    FVec bvec("bvec");
+    FVec cvec("cvec"); 
+    FVec retvec("retvec");
+    GenericAddress addr_a("a",0);
+    GenericAddress addr_b("b",0);
+    GenericAddress addr_c("c",0);
+    GenericAddress addr_ret("ret",0);
+
+    declareFVecFromFVec(ivector,avec);
+    declareFVecFromFVec(ivector,bvec);
+    declareFVecFromFVec(ivector,cvec);
+    declareFVecFromFVec(ivector,retvec);
+
+    loadFVec(ivector, avec, &addr_a, "");
+    loadFVec(ivector, bvec, &addr_b, "");
+    loadFVec(ivector, cvec, &addr_c, "");
+
+    fmaddFVec(ivector, retvec, avec, bvec, cvec);
+    storeFVec(ivector, retvec, &addr_ret, 0);
+    
+    out << "void testFMaddGenerated( " 
+        << FVecBaseType <<" *ret, "
+        << "const " << FVecBaseType <<" *a, "
+        << "const " << FVecBaseType <<" *b, " 
+        << "const " << FVecBaseType <<" *c )" << endl;
+
+    out << "{" << endl;
+
+    for(int i=0; i < ivector.size(); i++) {
+        out << ivector[i]->serialize() << endl;
+    }
+
+    out << "}" << endl;
+}
+
+void generate_Sub()
+{
+    ofstream out("intrinsic_fns/gen_sub.h");
+
+    InstVector ivector;  
+    FVec avec("avec");
+    FVec bvec("bvec");
+    FVec retvec("retvec");
+    GenericAddress addr_a("a",0);
+    GenericAddress addr_b("b",0);
+    GenericAddress addr_ret("ret",0);
+
+    declareFVecFromFVec(ivector,avec);
+    declareFVecFromFVec(ivector,bvec);
+    declareFVecFromFVec(ivector,retvec);
+
+    loadFVec(ivector, avec, &addr_a, "");
+    loadFVec(ivector, bvec, &addr_b, "");
+
+    subFVec(ivector, retvec, avec, bvec);
+    storeFVec(ivector, retvec, &addr_ret, 0);
+    
+    out << "void testSubGenerated( " 
+        << FVecBaseType <<" *ret, "
+        << "const " << FVecBaseType <<" *a, "
+        << "const " << FVecBaseType <<" *b )" << endl;
+
+    out << "{" << endl;
+
+    for(int i=0; i < ivector.size(); i++) {
+        out << ivector[i]->serialize() << endl;
+    }
+
+    out << "}" << endl;
+}
+
+
+void generate_Add()
+{
+    ofstream out("intrinsic_fns/gen_add.h");
+
+    InstVector ivector;  
+    FVec avec("avec");
+    FVec bvec("bvec");
+    FVec retvec("retvec");
+    GenericAddress addr_a("a",0);
+    GenericAddress addr_b("b",0);
+    GenericAddress addr_ret("ret",0);
+
+    declareFVecFromFVec(ivector,avec);
+    declareFVecFromFVec(ivector,bvec);
+    declareFVecFromFVec(ivector,retvec);
+
+    loadFVec(ivector, avec, &addr_a, "");
+    loadFVec(ivector, bvec, &addr_b, "");
+
+    addFVec(ivector, retvec, avec, bvec);
+    storeFVec(ivector, retvec, &addr_ret, 0);
+    
+    out << "void testAddGenerated( " 
+        << FVecBaseType <<" *ret, "
+        << "const " << FVecBaseType <<" *a, "
+        << "const " << FVecBaseType <<" *b )" << endl;
+
+    out << "{" << endl;
+
+    for(int i=0; i < ivector.size(); i++) {
+        out << ivector[i]->serialize() << endl;
+    }
+
+    out << "}" << endl;
+}
+
+void generate_Mul()
+{
+    ofstream out("intrinsic_fns/gen_mul.h");
+
+    InstVector ivector;  
+    FVec avec("avec");
+    FVec bvec("bvec");
+    FVec retvec("retvec");
+    GenericAddress addr_a("a",0);
+    GenericAddress addr_b("b",0);
+    GenericAddress addr_ret("ret",0);
+
+    declareFVecFromFVec(ivector,avec);
+    declareFVecFromFVec(ivector,bvec);
+    declareFVecFromFVec(ivector,retvec);
+
+    loadFVec(ivector, avec, &addr_a, "");
+    loadFVec(ivector, bvec, &addr_b, "");
+
+    mulFVec(ivector, retvec, avec, bvec);
+    storeFVec(ivector, retvec, &addr_ret, 0);
+    
+    out << "void testMulGenerated( " 
+        << FVecBaseType <<" *ret, "
+        << "const " << FVecBaseType <<" *a, "
+        << "const " << FVecBaseType <<" *b )" << endl;
+
+    out << "{" << endl;
+
+    for(int i=0; i < ivector.size(); i++) {
+        out << ivector[i]->serialize() << endl;
+    }
+
+    out << "}" << endl;
+}
+
+void generate_SetZero()
+{
+    ofstream out("intrinsic_fns/gen_setzero.h");
+
+    InstVector ivector;
+    FVec retvec("retvec");
+    GenericAddress addr_ret("ret",0);
+
+    declareFVecFromFVec(ivector,retvec);
+
+    setZero(ivector, retvec);
+    storeFVec(ivector, retvec, &addr_ret, 0);
+    
+    out << "void testSetZeroGenerated( " 
+        << FVecBaseType <<" *ret )" << endl;
+
+    out << "{" << endl;
+
+    for(int i=0; i < ivector.size(); i++) {
+        out << ivector[i]->serialize() << endl;
+    }
+
+    out << "}" << endl;
+}
+
+
 int main(int argc, char *argv[])
 {
     create_test_dir();
     generate_FnMadd();
+    generate_FMadd();
+    generate_Sub();
+    generate_Add();
+    generate_Mul();
+    generate_SetZero();
 }
--- testsuite/run_tests.cc
+++ testsuite/run_tests.cc
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <iomanip> 
 #include <cmath>
 using namespace std;
 
-
+// FIXME - Include the correct header based on the arch.
 #include <immintrin.h>
 
 #if PRECISION == 1
@@ -14,9 +15,154 @@ using namespace std;
 #endif
 
 
+// Generated functions that use the compiler intrinsics
+#include "intrinsic_fns/gen_setzero.h"
+#include "intrinsic_fns/gen_add.h"
+#include "intrinsic_fns/gen_sub.h"
+#include "intrinsic_fns/gen_mul.h"
+#include "intrinsic_fns/gen_fnMadd.h"
+#include "intrinsic_fns/gen_fMadd.h"
 
-#include "test_gen_fnMadd.h"
 
+// Test the add function
+int test_setZero()
+{
+  FVecBaseType ret[VECLEN];
+  FVecBaseType ret2[VECLEN];
+  
+  cout << setw(10) << left << "SetZero" << ": " ;
+  // Call generated function
+  testSetZeroGenerated(ret);
+
+  // Do it manually
+  for(int i=0; i < VECLEN; i++) { 
+    ret2[i] = 0.0;
+  }
+  
+  for(int i=0; i < VECLEN; i++) { 
+    double diff = ret2[i]-ret[i];
+    double rel_diff = diff/ret[2];
+    if( fabs(rel_diff) > tol ) { 
+      std::cout << "FAIL: i= " << i << " desired = " 
+                << ret2[i] << " generated=" << ret[i] << endl;
+      return 0;
+    }
+  }
+ 
+  std::cout << "PASS" << endl;
+  return 1;
+}
+
+// Test the add function
+int test_add()
+{
+  FVecBaseType a[VECLEN];
+  FVecBaseType b[VECLEN];
+  FVecBaseType ret[VECLEN];
+  FVecBaseType ret2[VECLEN];
+  
+  for(int i=0; i < VECLEN; i++) {
+    a[i] = (FVecBaseType)(i+1);
+    b[i] = (FVecBaseType)(VECLEN*i+2);
+  }
+  
+  cout << setw(10) << left << "Add" << ": ";
+  // Call generated function
+  testAddGenerated(ret,a,b);
+
+  // Do it manually
+  for(int i=0; i < VECLEN; i++) { 
+    ret2[i] = a[i] + b[i];
+  }
+  
+  for(int i=0; i < VECLEN; i++) { 
+    double diff = ret2[i]-ret[i];
+    double rel_diff = diff/ret[2];
+    if( fabs(rel_diff) > tol ) { 
+      std::cout << "FAIL: i= " << i << " desired = " 
+                << ret2[i] << " generated=" << ret[i] << endl;
+      return 0;
+    }
+  }
+ 
+  std::cout << "PASS" << left << endl;
+  return 1;
+}
+
+// Test the add function
+int test_sub()
+{
+  FVecBaseType a[VECLEN];
+  FVecBaseType b[VECLEN];
+  FVecBaseType ret[VECLEN];
+  FVecBaseType ret2[VECLEN];
+  
+  for(int i=0; i < VECLEN; i++) {
+    a[i] = (FVecBaseType)(i+1);
+    b[i] = (FVecBaseType)(VECLEN*i+2);
+  }
+  
+  cout << setw(10) << left << "Sub" << ": " ;
+  // Call generated function
+  testSubGenerated(ret,a,b);
+
+  // Do it manually
+  for(int i=0; i < VECLEN; i++) { 
+    ret2[i] = a[i] - b[i];
+  }
+  
+  for(int i=0; i < VECLEN; i++) { 
+    double diff = ret2[i]-ret[i];
+    double rel_diff = diff/ret[2];
+    if( fabs(rel_diff) > tol ) { 
+      std::cout << "FAIL: i= " << i << " desired = " 
+                << ret2[i] << " generated=" << ret[i] << endl;
+      return 0;
+    }
+  }
+ 
+  std::cout << "PASS" << endl;
+  return 1;
+}
+
+// Test the add function
+int test_mul()
+{
+  FVecBaseType a[VECLEN];
+  FVecBaseType b[VECLEN];
+  FVecBaseType ret[VECLEN];
+  FVecBaseType ret2[VECLEN];
+  
+  for(int i=0; i < VECLEN; i++) {
+    a[i] = (FVecBaseType)(i+1);
+    b[i] = (FVecBaseType)(VECLEN*i+2);
+  }
+  
+  cout << setw(10) << left << "Mul" << ": " ;
+  // Call generated function
+  testMulGenerated(ret,a,b);
+
+  // Do it manually
+  for(int i=0; i < VECLEN; i++) { 
+    ret2[i] = a[i] * b[i];
+  }
+  
+  for(int i=0; i < VECLEN; i++) { 
+    double diff = ret2[i]-ret[i];
+    double rel_diff = diff/ret[2];
+    if( fabs(rel_diff) > tol ) { 
+      std::cout << "FAIL: i= " << i << " desired = " 
+                << ret2[i] << " generated=" << ret[i] << endl;
+      return 0;
+    }
+  }
+ 
+  std::cout << "PASS" << endl;
+  return 1;
+}
+
+
+// Testing the fnMadd function
 int test_fnMadd()
 {
   FVecBaseType a[VECLEN];
@@ -24,7 +170,9 @@ int test_fnMadd()
   FVecBaseType c[VECLEN];
   FVecBaseType ret[VECLEN];
   FVecBaseType ret2[VECLEN];
-  
+
+  cout << setw(10) << left << "FnMAdd" << ": " ;
+
   for(int i=0; i < VECLEN; i++) {
     a[i] = (FVecBaseType)(i+1);
     b[i] = (FVecBaseType)(VECLEN*i+2);
@@ -43,7 +191,8 @@ int test_fnMadd()
     double diff = ret2[i]-ret[i];
     double rel_diff = diff/ret[2];
     if( fabs(rel_diff) > tol ) { 
-      std::cout << "FAIL: i= " << i << " desired = " << ret2[i] << " generated=" << ret[i] << endl;
+      std::cout << "FAIL: i= " << i << " desired = " 
+                << ret2[i] << " generated=" << ret[i] << endl;
       return 0;
     }
   }
@@ -53,7 +202,55 @@ int test_fnMadd()
 }
 
 
+// Testing the fnMadd function
+int test_fMadd()
+{
+  FVecBaseType a[VECLEN];
+  FVecBaseType b[VECLEN];
+  FVecBaseType c[VECLEN];
+  FVecBaseType ret[VECLEN];
+  FVecBaseType ret2[VECLEN];
+
+  cout << setw(10) << left << "FMAdd" << ": " ;
+
+  for(int i=0; i < VECLEN; i++) {
+    a[i] = (FVecBaseType)(i+1);
+    b[i] = (FVecBaseType)(VECLEN*i+2);
+    c[i] = (FVecBaseType)(2*VECLEN*i+3);
+  }
+  
+  // Call generated function
+  testFMaddGenerated(ret,a,b,c);
+
+  // Do it manually
+  for(int i=0; i < VECLEN; i++) { 
+    ret2[i] = c[i] + (a[i]*b[i]);
+  }
+  
+  for(int i=0; i < VECLEN; i++) { 
+    double diff = ret2[i]-ret[i];
+    double rel_diff = diff/ret[2];
+    if( fabs(rel_diff) > tol ) { 
+      std::cout << "FAIL: i= " << i << " desired = " 
+                << ret2[i] << " generated=" << ret[i] << endl;
+      return 0;
+    }
+  }
+ 
+  std::cout << "PASS" << endl;
+  return 1;
+}
+
 int main(int argc, char *argv[]) 
 {
-  if ( ! test_fnMadd() ) { std::cout << "TEST FAILED" << endl; abort(); }
+  test_setZero();
+  test_add();
+  test_sub();
+  test_mul();
+  test_fnMadd();
+  test_fMadd();
+
+  // if ( ! test_fnMadd() ) { 
+  //    std::cout << "TEST FAILED" << endl; abort(); 
+  //}
 }
