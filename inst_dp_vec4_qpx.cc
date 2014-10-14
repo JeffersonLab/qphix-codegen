b7c800a3fe07bdecbfdaf5e036b35ddf30e92b6b
Diptorup Deb <diptorup@cs.unc.edu>
2014-10-13 22:20:31 -0400

    Cleaned up version.
            -- QPX would not support gather,scatter or halftypes.
            -- Removed the dead code (copied over from avx).

--- inst_dp_vec4_qpx.cc
+++ inst_dp_vec4_qpx.cc
@@ -16,14 +16,13 @@ FVec::FVec(const string& name_) : name(name_), type(FVECTYPE) {}
 
 string DeclareFVec::serialize() const
 {
-    return v.getType()+" "+v.getName()+ " = vec_splats(0.0);";
+    return v.getType() + " " + v.getName() + " = (0.0);";
 }
-
-
 
 string InitFVec::serialize() const
 {
-    return v.getName()+ " = vec_splats(0.0);";
+    //return v.getName()+ " = vec_splats(0.0);";
+    return "";
 }
 
 
@@ -48,18 +47,9 @@ string IntToMask::serialize() const
     return outbuf.str();
 }
 
-/*
-string DeclareOffsets::serialize() const
-{
-    ostringstream outbuf;
-    outbuf << "__m128i " << vname << " = _mm_load_si128((__m128i const *)" << pname << ");" << endl;
-    return outbuf.str();
-}
-*/
-
 string IfAllOneCond::serialize() const
 {
-    return " if ((" + condition + " & " + fullIntMask + ") == " + fullIntMask + ") { ";
+    return "if((" + condition + " & " + fullIntMask + ") ""== " + fullIntMask + ") { ";
 }
 
 string LoadFVec::serialize() const
@@ -68,24 +58,29 @@ string LoadFVec::serialize() const
 
     if(mask.empty()) {
         if(!a->isHalfType()) {
-            buf << v.getName() << " = vec_ld(0, " << a->serialize() << ");" <<endl;
+            buf << v.getName() << " = vec_ld(0, " << a->serialize() << ");" 
+                << endl;
         }
         else {
             //FIXME
-            buf << "#error \"halftype is not supported (yet) in the QPX backend\"" 
+            buf << "#error \"halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\"" 
                 << endl;
-            printf("FIXME: halftype is not supported (yet) in the QPX backend\n");
+            printf("FIXME: halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\n");
             exit(1);
-
-            //buf << v.getName() << " = _mm256_cvtps_pd(_mm_load_ps(" << a->serialize() << "));" <<endl;
         }
     }
     else {
         if(!a->isHalfType()) {
+        
             /* Define a zero vector. */
-            buf << v.getType() << " zeroVec = vec_splats(0.0);" << endl; 
+            buf << v.getType() << " zeroVec = (0.0);" << endl; 
+            
             /* Load the vector from memory. */
-            buf << v.getName() << " = vec_ld(0, " << a->serialize() << ");" << endl;
+            buf << v.getName() << " = vec_ld(0, " << a->serialize() << ");" 
+                << endl;
+                
             /* Blend with the zero vector. Retain the elements that have the 
              * corresponding high bit set in the mask. 
              */
@@ -94,11 +89,12 @@ string LoadFVec::serialize() const
         }
         else {
             //FIXME
-            buf << "#error \"halftype is not supported (yet) in the QPX backend\"" 
+            buf << "#error \"halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\"" 
                 << endl;
-            printf("FIXME: halftype is not supported (yet) in the QPX backend\n");
+            printf("FIXME: halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\n");
             exit(1);
-            //buf << v.getName() << " = _mm256_cvtps_pd(_mm_maskload_ps(" << a->serialize() << ", _mm_castps_si128(_mm256_cvtpd_ps(" << mask << "))));" <<endl;
         }
     }
 
@@ -112,72 +108,37 @@ string StoreFVec::serialize() const
     //int streaming = isStreaming;
     //TODO : Does QPX have an equivalent of AVX _mm256_stream_pd?
     if(!a->isHalfType()) {
-      buf << "vec_sta("  << v.getName() << ", 0, " << a->serialize() << ");" << endl;
+      buf << "vec_sta("  << v.getName() << ", 0, " << a->serialize() << ");" 
+          << endl;
     }
     else {
         //FIXME
-        buf << "#error \"halftype is not supported (yet) in the QPX backend\"" 
+        buf << "#error \"halftype is not supported in the inst_dp_vec4_qpx "
+               "backend\"" 
             << endl;
-        printf("FIXME: halftype is not supported (yet) in the QPX backend\n");
+        printf("FIXME: halftype is not supported in the inst_dp_vec4_qpx "
+               "backend\n");
         exit(1);
-
-        //buf << "_mm_stream_ps(" << a->serialize() << ", _mm256_cvtpd_ps(" << v.getName() <<  "));" <<endl;
     }
     return buf.str();
 }
 
-// FIXME : Not being used for dslash AVX backend. Not needed for QPX as well?
-/*
-string GatherFVec::serialize() const
-{
-    std::ostringstream buf;
-
-    if(mask.empty()) {
-        if(!a->isHalfType()) {
-            buf << v.getName() << " = _mm256_i32gather_pd(" << a->getBase()  << ", " << a->getOffsets() << ", _MM_SCALE_8);" <<endl;
-        }
-        else {
-            buf << v.getName() << " = _mm256_cvtps_pd(_mm_i32gather_ps(" << a->getBase()  << ", " << a->getOffsets() << ", _MM_SCALE_4));" <<endl;
-        }
-    }
-    else {
-        if(!a->isHalfType()) {
-            buf << v.getName() << " = _mm256_mask_i32gather_pd(" << v.getName() << ", " << a->getBase()  << ", " << a->getOffsets() << ", " << mask << ", _MM_SCALE_8);" <<endl;
-        }
-        else {
-            buf << v.getName() << " = _mm256_cvtps_pd(_mm__mask_i32gather_ps(" << v.getName() << ", " << a->getBase()  << ", " << a->getOffsets() << ", " << mask << ", _MM_SCALE_4));" <<endl;
-        }
-    }
-
-    return buf.str();
-
-}
-
-string ScatterFVec::serialize() const
-{
-    std::ostringstream buf;
-    // FIXME : Confirm that the below error message is true
-    buf << "#error \"scatter is not supported in QPX\"" <<endl;
-    printf("scatter is not supported in QPX\n");
-    exit(1);
-    return buf.str();
-}
-*/
 string LoadBroadcast::serialize() const
 {
     std::ostringstream buf;
 
     if(!a->isHalfType()) {
-        buf << v.getName() << " = vec_splats(*" << a->serialize() << ");" << endl;
+        buf << v.getName() << " = vec_splats(*" << a->serialize() << ");" 
+            << endl;
     }
     else {
         //FIXME
-        buf << "#error \"halftype is not supported (yet) in the QPX backend\"" 
+        buf << "#error \"halftype is not supported in the inst_dp_vec4_qpx "
+               "backend\"" 
             << endl;
-        printf("FIXME: halftype is not supported (yet) in the QPX backend\n");
+        printf("FIXME: halftype is not supported in the inst_dp_vec4_qpx "
+               "backend\n");
         exit(1);
-
-        //buf << v.getName() << " = _mm256_cvtps_pd(_mm_broadcast_ss(" << a->serialize() << "));" << endl;
     }
 
     return buf.str();
@@ -237,56 +198,7 @@ PrefetchL2::PrefetchL2( const Address* a_, int type) : a(a_)
         break;
     }
 }
-/*
-GatherPrefetchL1::GatherPrefetchL1( const GatherAddress* a_, int type) : a(a_)
-{
-    // Type: 0 - none, 1 - NT, 2 - Ex, 3 - NT+Ex
-    switch (type) {
-    case 0:
-        hint = "_MM_HINT_T0";
-        break;
 
-    case 1:
-        hint = "_MM_HINT_NTA";
-        break;
-//        case 2: hint = "_MM_HINT_ET0"; break;
-//        case 3: hint = "_MM_HINT_ENTA"; break;
-    }
-}
-string GatherPrefetchL1::serialize() const
-{
-    std::ostringstream buf;
-    buf << "#error \"Gather Prefetch is not supported in AVX/AVX2\"" <<endl;
-    printf("Gather Prefetch is not supported in AVX/AVX2\n");
-    exit(1);
-    return buf.str();
-}
-
-GatherPrefetchL2::GatherPrefetchL2( const GatherAddress* a_, int type) : a(a_)
-{
-    // Type: 0 - none, 1 - NT, 2 - Ex, 3 - NT+Ex
-    switch(type) {
-    case 0:
-        hint = "_MM_HINT_T1";
-        break;
-
-    case 1:
-        hint = "_MM_HINT_T2";
-        break;
-//        case 2: hint = "_MM_HINT_ET1"; break;
-//        case 3: hint = "_MM_HINT_ET2"; break;
-    }
-}
-string GatherPrefetchL2::serialize() const
-{
-    std::ostringstream buf;
-    buf << "#error \"Gather Prefetch is not supported in AVX/AVX2\"" <<endl;
-    printf("Gather Prefetch is not supported in AVX/AVX2\n");
-    exit(1);
-    return buf.str();
-
-}
-*/
 string SetZero::serialize() const
 {
     return  ret.getName() + " = vec_splats(0.0); ";
@@ -411,70 +323,7 @@ private:
     int imm;
 };
 */
-//FIXME : One we add support for HalfFVec to QPX backend
-/*
-class LoadHalfFVec : public MemRefInstruction
-{
-public:
-    LoadHalfFVec( const FVec& v_, const Address* a_, const int num_) : v(v_), a(a_), num(num_) {}
-    string serialize() const
-    {
-        std::ostringstream buf;
 
-        if(!a->isHalfType()) {
-            buf << v.getName() << " =  _mm256_insertf128_pd(" << v.getName() << ", _mm_load_pd(" << a->serialize() << "), " << num << ");" << endl;
-        }
-        else {
-            buf << v.getName() << " =  _mm256_insertf128_pd(" << v.getName() << ", _mm_cvtps_pd(_mm_castpd_ps(_mm_load_sd((double*)" << a->serialize() << "))), " << num << ");" << endl;
-        }
-
-        return buf.str();
-    }
-    const Address* getAddress() const
-    {
-        return a;
-    }
-    MemRefType getType() const
-    {
-        return LOAD_MASKED_VEC;
-    }
-private:
-    const FVec v;
-    const Address* a;
-    const int num;
-};
-
-class StoreHalfFVec : public MemRefInstruction
-{
-public:
-    StoreHalfFVec( const FVec& v_, const Address* a_, const int num_) : v(v_), a(a_), num(num_) {}
-    string serialize() const
-    {
-        std::ostringstream buf;
-
-        if(!a->isHalfType()) {
-            buf << "_mm_store_pd(" << a->serialize() <<  ", _mm256_extractf128_pd(" << v.getName() << ", " << num << "));" << endl;
-        }
-        else {
-            buf << "_mm_store_sd((double*)" << a->serialize() <<  ", _mm_castps_pd(_mm_cvtpd_ps(_mm256_extractf128_pd(" << v.getName() << ", " << num << "))));" << endl;
-        }
-
-        return buf.str();
-    }
-    const Address* getAddress() const
-    {
-        return a;
-    }
-    MemRefType getType() const
-    {
-        return LOAD_MASKED_VEC;
-    }
-private:
-    const FVec v;
-    const Address* a;
-    const int num;
-};
-*/
 class LoadSplitSOAFVec : public MemRefInstruction
 {
 public:
@@ -496,70 +345,60 @@ public:
                 if(soalen == 4) {
                     buf << v.getName() 
                         << " =  vec_perm(" 
-                        << "vec_ld(" << a1->serialize() << "), "
+                        << "vec_ld(0, " << a1->serialize() << "), "
                         << "vec_splats(*" << a2->serialize() << "), "
                         << "_v4d_int2mask("
                         << (1 << (soalen-1)) << "));" 
                         << endl;
                 }
                 else {
-                     buf << "#error\"soalen 2 is not supported (yet) in the QPX backend\"" <<endl;
-                     printf("FIXME: soalen 2 is not supported (yet) in the QPX backend\n");
+                     buf << "#error\"soalen 2 is not supported in "
+                            "the QPX backend\"" <<endl;
+                     printf("FIXME: soalen 2 is not supported in "
+                            "the QPX backend\n");
                      exit(1);
                      //FIXME
-                     //buf << v.getName() << " =  _mm256_insertf128_pd(" << v.getName() <<
-                     //   ", _mm_blend_pd(_mm_loadu_pd(" << a1->serialize() << "), _mm_loaddup_pd(" << a2->serialize() << "), " << (1 << (soalen-1)) << "), " << soanum << ");" << endl;
+                     //buf << v.getName() << " =  _mm256_insertf128_pd(" 
+                     //    << v.getName() << ", _mm_blend_pd(_mm_loadu_pd(" 
+                     //    << a1->serialize() << "), _mm_loaddup_pd(" 
+                     //    << a2->serialize() << "), " << (1 << (soalen-1)) 
+                     //    << "), " << soanum << ");" 
+                     //    << endl;
                 }
             }
             else {
                 if(soalen == 4) {
                     buf << v.getName() 
                         << " =  vec_perm("
-                        << "vec_ld((" << a2->serialize() << ")-1), "
+                        << "vec_ld(0, (" << a2->serialize() << ")-1), "
                         << "vec_splats(*" << a1->serialize() << "), "
                         << " _v4d_int2mask(1));" 
                         << endl;
                 }
                 //FIXME
                 else {
-                    buf << "#error\"soalen 2 is not supported (yet) in the QPX backend\"" <<endl;
-                    printf("FIXME: soalen 2 is not supported (yet) in the QPX backend\n");
+                    buf << "#error\"soalen 2 is not supported in " 
+                           "the QPX backend\"" 
+                        << endl;
+                    printf("FIXME: soalen 2 is not supported in " 
+                           "the QPX backend\n");
                     exit(1);
-                    //buf << v.getName() << " =  _mm256_insertf128_pd(" << v.getName() <<
-                    //    ", _mm_blend_pd(_mm_loadu_pd((" << a2->serialize() << ")-1), _mm_loaddup_pd(" << a1->serialize() << "), 1), " << soanum << ");" << endl;
+                    //buf << v.getName() << " =  _mm256_insertf128_pd(" 
+                    //    << v.getName() << ", _mm_blend_pd(_mm_loadu_pd((" 
+                    //    << a2->serialize() << ")-1), _mm_loaddup_pd(" 
+                    //    << a1->serialize() << "), 1), " 
+                    //    << soanum << ");" << endl;
                 }
             }
         }
         else {
-            buf << "#error \"halftype is not supported (yet) in the QPX backend\"" 
+            buf << "#error \"halftype is not supported in "
+                   "the QPX backend\"" 
                 << endl;
-            printf("FIXME: halftype is not supported (yet) in the QPX backend\n");
+            printf("FIXME: halftype is not supported in " 
+                   "the QPX backend\n");
             exit(1);
-
-            //FIXME
-            /*
-            if(forward) {
-                if(soalen == 4) {
-                    buf << v.getName() << " =  _mm256_cvtps_pd(_mm_blend_ps(_mm_loadu_ps(" << a1->serialize() << "), _mm_broadcast_ss(" <<
-                        a2->serialize() << "), " << (1 << (soalen-1)) << "));" << endl;
-                }
-                else {
-                    buf << v.getName() << " =  _mm256_insertf128_pd(" << v.getName() <<
-                        ", _mm_cvtps_pd(_mm_blend_ps(_mm_loadu_ps(" << a1->serialize() << "), _mm_broadcast_ss(" << a2->serialize() << "), " << (1 << (soalen-1)) << ")), " << soanum << ");" << endl;
-                }
-            }
-            else {
-                if(soalen == 4) {
-                    buf << v.getName() << " =  _mm256_cvtps_pd(_mm_blend_ps(_mm_loadu_ps((" << a2->serialize() << ")-1), _mm_broadcast_ss(" << a1->serialize() << "), 1));" << endl;
-                }
-                else {
-                    buf << v.getName() << " =  _mm256_insertf128_pd(" << v.getName() <<
-                        ", _mm_cvtps_pd(_mm_blend_ps(_mm_loadu_ps((" << a2->serialize() << ")-1), _mm_broadcast_ss(" << a1->serialize() << "), 1)), " << soanum << ");" << endl;
-                }
-            } 
-            */
         }
-
         return buf.str();
     }
     
@@ -609,12 +448,12 @@ public:
         }
         else {
             //FIXME
-            buf << "#error \"halftype is not supported (yet) in the QPX backend\"" 
+            buf << "#error \"halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\"" 
                 << endl;
-            printf("FIXME: halftype is not supported (yet) in the QPX backend\n");
+            printf("FIXME: halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\n");
             exit(1);
-
-            //buf << v.getName() << " = _mm256_blend_pd(" << v.getName() << ", _mm256_cvtps_pd(_mm_broadcast_ss(" << a->serialize() << ")), " << (1<<pos) << ");" << endl;
         }
 
         return buf.str();
@@ -655,24 +494,28 @@ public:
 
         if(!a->isHalfType()) {
             if(pos % 2 == 0) {
-                //TODO
+                //TODO/FIXME : convert ot qpx
                 buf << "vec_st(" << a->serialize() << ", 0, "
-                    << "_mm256_extractf128_pd(" << v.getName() << ", " << pos / 2 << ")"
+                    << "_mm256_extractf128_pd(" << v.getName() << ", " 
+                    << pos / 2 << ")"
                     << ");" << endl;
             }
             else {
                 //TODO
-                buf << "_mm_storeh_pd(" << a->serialize() << ", _mm256_extractf128_pd(" << v.getName() << ", " << pos / 2 << "));" << endl;
+                buf << "_mm_storeh_pd(" << a->serialize() 
+                    << ", _mm256_extractf128_pd(" << v.getName() << ", " 
+                    << pos / 2 << "));" << endl;
             }
+             
         }
         else {
             //FIXME
-            buf << "#error \"halftype is not supported (yet) in the QPX backend\"" 
+            buf << "#error \"halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\"" 
                 << endl;
-            printf("FIXME: halftype is not supported (yet) in the QPX backend\n");
+            printf("FIXME: halftype is not supported in the inst_dp_vec4_qpx "
+                   "backend\n");
             exit(1);
-
-            //buf << "((int*)" << a->serialize() << ")[0] = _mm_extract_ps(_mm256_cvtpd_ps(" << v.getName() << "), " << pos << ");" << endl;
         }
 
         return buf.str();
@@ -724,7 +567,7 @@ void storeSOAFVec(InstVector& ivector
     if(soalen == 4) {
         ivector.push_back( new StoreFVec(ret, a, 0));
     }
-    //FIXME : Add support for SOAlen == 2
+    //FIXME : Add support for soalen == 2
     /*
     else if(soalen == 2) {
         ivector.push_back( new StoreHalfFVec(ret, a, soanum));
@@ -798,28 +641,7 @@ void packFVec(InstVector& ivector
         }
     }
 }
-/*
-void gatherFVec(InstVector& ivector, const FVec& ret, GatherAddress *a, string mask)
-{
-    ivector.push_back( new GatherFVec(ret, a, mask));
-}
 
-void scatterFVec(InstVector& ivector, const FVec& ret, GatherAddress *a)
-{
-    ivector.push_back( new ScatterFVec(ret, a));
-}
-*/
-/*
-void gatherPrefetchL1(InstVector& ivector, GatherAddress *a, int type)
-{
-    ivector.push_back( new GatherPrefetchL1(a, type));
-}
-
-void gatherPrefetchL2(InstVector& ivector, GatherAddress *a, int type)
-{
-    ivector.push_back( new GatherPrefetchL2(a, type));
-}
-*/
 /*
 void fperm64x2(InstVector& ivector
              , const FVec& ret
@@ -848,7 +670,8 @@ void transpose(InstVector& ivector, const FVec r[], const FVec f[], int soalen)
     case 2:
         // FIXME : Add support for SOALEN 2 
         //transpose2x2(ivector, r, f);
-        printf("SOALEN = %d Not Yet supported (only SOALEN = 4 supported)\n", soalen); 
+        printf("SOALEN = %d Not Yet supported " 
+               "(only SOALEN = 4 supported)\n", soalen); 
         break;
 
     case 4:
@@ -856,8 +679,8 @@ void transpose(InstVector& ivector, const FVec r[], const FVec f[], int soalen)
         break;
 
     default:
-        printf("SOALEN = %d Not Yet supported (only SOALEN = 4 supported)\n", soalen); 
-        //printf("SOALEN = %d Not Supported (only SOALEN = 2, 4 supported)\n", soalen);
+        printf("SOALEN = %d Not Yet supported "
+               "(only SOALEN = 4 supported)\n", soalen); 
     }
 }
 
